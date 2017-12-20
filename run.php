b184ef1aeb14eb7dcec0b574809bc2aa04303f8d
igo4ek <34678989+igo4ek@users.noreply.github.com>
2017-12-20 19:57:08 +0300

    Update run.php

--- run.php
+++ run.php
@@ -7,8 +7,8 @@ class ProblemSolver
     public static function BracesBalance($inputString)
     {
         $counter=0;
-		for($i=0;$i<strlen($inputString);$i++)
-		{
+	for($i=0;$i<strlen($inputString);$i++)
+	{
             if($inputString[$i]==')')$counter--;
             if($inputString[$i]=='(')$counter++;
             if($counter<0)return false;
