9a75a85ad78db6637acd10090e4db9b641acaa4a
igo4ek <34678989+igo4ek@users.noreply.github.com>
2017-12-20 19:44:56 +0300

    bracers

---
+++ run.php
@@ -0,0 +1,23 @@
+
+
+<?php
+
+class ProblemSolver
+{
+    public static function BracesBalance($inputString)
+    {
+        $counter=0;
+		for($i=0;$i<strlen($inputString);$i++)
+		{
+            if($inputString[$i]==')')$counter--;
+            if($inputString[$i]=='(')$counter++;
+            if($counter<0)return false;
+        }
+        if($counter==0)return true;
+    }
+}
+//foreach($argv as $a) echo $a."\r\n";
+echo ProblemSolver::BracesBalance($argv[1])? "OK": "FAIL";
+
+?>
+
